b8c314013dd193396de2d2a3dc1d2f58ae7ff3bd
听着情歌流泪 <develop@lingtal.com>
2019-09-09 13:43:01 +0800

    feat: 新增commit规范
    新增commit规范

---
+++ .commitlintrc.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  extends: ['@commitlint/config-conventional', 'cz'],
+  rules: {
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'art', 'zap', 'remove']]
+  }
+}
